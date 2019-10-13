fa0734c64a530bb028e39617a543c0107d703c72
Waloid <andreika8678@gmail.com>
2019-10-13 16:07:00 +0300

    lol 3

--- lol.js
+++ lol.js
@@ -21,11 +21,11 @@ let appData = {
 
 function chooseExpenses() {                  //Статья обязательных расходов
     for (let i = 0; i < 2; i++){
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-            b = prompt("Во сколько обойдется?", '');
+        let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+        let b = prompt("Во сколько обойдется?", '');
     
-        if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
-            && a != '' && b != '' && a.length > 50){
+        if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null &&
+            a != '' && b != '' && a.length > 50){
             console.log ("done");
             appData.expenses[a] = b;
         }  else {
